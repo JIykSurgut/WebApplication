d3451b40cab833ce02dd467f78cee3c825057c7c
JIyk <sbis86@mail.ru>
2017-07-26 14:50:07 +0500

    1

---
+++ WebApp/Models/Identity/Role/AppRole.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+
+namespace Models
+{
+    public class AppRole : IRole<int>
+    {
+        public int Id { get; set; }      //Идентификатор
+        public string Name { get; set; }    //Имя роли
+
+        public AppRole()
+        {
+            Id = 0; //отсутствие ID
+        }
+        public AppRole(string name) : this()
+        {
+            Name = name;
+        }
+        public AppRole(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
