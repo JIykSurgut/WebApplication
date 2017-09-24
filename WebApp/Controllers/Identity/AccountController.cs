a6786d92738f06bb20ab519a237097aa6ecf63f1
sbis86 <sbis86@mail.ru>
2017-09-24 23:57:30 +0500

    google

--- WebApp/Controllers/Identity/AccountController.cs
+++ WebApp/Controllers/Identity/AccountController.cs
@@ -6,7 +6,6 @@ using Models;
 using Microsoft.Owin.Security;
 using Microsoft.AspNet.Identity;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Controllers
 {
@@ -275,6 +274,36 @@ namespace Controllers
             foreach (var error in result.Errors)
             {
                 ModelState.AddModelError("", error);
+            }
+        }
+
+        private const string XsrfKey = "XsrfId";
+        internal class ChallengeResult : HttpUnauthorizedResult
+        {
+            public ChallengeResult(string provider, string redirectUri)
+                : this(provider, redirectUri, null)
+            {
+            }
+
+            public ChallengeResult(string provider, string redirectUri, string userId)
+            {
+                LoginProvider = provider;
+                RedirectUri = redirectUri;
+                UserId = userId;
+            }
+
+            public string LoginProvider { get; set; }
+            public string RedirectUri { get; set; }
+            public string UserId { get; set; }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
+                if (UserId != null)
+                {
+                    properties.Dictionary[XsrfKey] = UserId;
+                }
+                context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
             }
         }
         #endregion
